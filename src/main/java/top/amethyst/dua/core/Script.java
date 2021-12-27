98d60674dbe6e5da367f9b5016dddd4a497ac9e8
Chlamydomonos <2457242458@qq.com>
2021-12-28 02:01:34 +0800

    Test dependencies

--- src/main/java/top/amethyst/dua/core/Script.java
+++ src/main/java/top/amethyst/dua/core/Script.java
@@ -3,12 +3,31 @@ package top.amethyst.dua.core;
 import com.google.gson.JsonObject;
 import org.jetbrains.annotations.NotNull;
 import top.amethyst.dua.api.core.IJsonSerializable;
+import xyz.chlamydomonos.brainfuc.BFChScript;
 
-public class Script implements IJsonSerializable
+import java.util.ArrayList;
+
+public class Script extends BFChScript implements IJsonSerializable
 {
+    public Script(@NotNull String data)
+    {
+        super(data);
+    }
+
+    public Script(@NotNull JsonObject json)
+    {
+        this(json.get("data").getAsString());
+    }
+
     @Override
     public @NotNull JsonObject serialize()
     {
-        return new JsonObject();
+        JsonObject json = new JsonObject();
+        ArrayList<Character> temp = getData();
+        StringBuilder builder = new StringBuilder();
+        for(char i : temp)
+            builder.append(i);
+        json.addProperty("data", builder.toString());
+        return json;
     }
 }
